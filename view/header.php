c92c66c4779367c7343a729fa8dd873f1d75d934
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2020-11-20 17:53:14 -0800

    Initial commit

---
+++ view/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+
+<head>
+  <title>Scheduling App</title>
+  <link rel="stylesheet" type="text/css" href="/book_apps/ch05_guitar_shop/main.css">
+</head>
+
+<!-- the body section -->
+
+<body>
+  <header>
+    <h1>Scheduling App</h1>
+  </header>
+
+</body>
+
+</html>
