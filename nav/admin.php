469e4c73a8fbb6970f1b6ca638b0e92fdec740b0
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2020-12-11 22:20:51 -0800

    Implemented account navbars

---
+++ nav/admin.php
@@ -0,0 +1,46 @@
+<?php
+
+$curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <div class="d-flex flex-grow-1">
+            <span class="w-100 d-lg-none d-block">
+                <!-- hidden spacer to center brand on mobile --></span>
+            <a class="navbar-brand d-none d-lg-inline-block" href="index.php">
+                Scheduling App
+            </a>
+            <a class="navbar-brand-two mx-auto d-lg-none d-inline-block" href="#">
+                <!-- <img src="//placehold.it/40?text=LOGO" alt="logo"> -->
+                <span class="navbar-brand">Scheduling App</span>
+            </a>
+            <div class="w-100 text-right">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse flex-grow-1 text-right">
+            <ul class="navbar-nav ml-auto flex-nowrap">
+                <li class="nav-item">
+                    <a href="appointments" class="nav-link m-2 menu-item <?php if ($curPageName == 'appointments.php') echo 'active' ?>">Appointments</a>
+                </li>
+                <li class="nav-item">
+                    <a href="calendar" class="nav-link m-2 menu-item <?php if ($curPageName == 'calendar.php') echo 'active' ?>">Calendar</a>
+                </li>
+                <li class="nav-item">
+                    <a href="settings" class="nav-link m-2 menu-item <?php if ($curPageName == 'settings.php') echo 'active' ?>">Settings</a>
+                </li>
+                <li class="nav-item m-2">
+                    <form action="" method="post">
+                        <button class="btn btn-danger my-2 my-sm-0">
+                            Sign Out
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- view/header.php
+++ view/header.php
@@ -1,3 +1,9 @@
+<?php
+
+$login = true;
+$admin = false;
+
+?>
 <!DOCTYPE html>
 <html>
 <!-- the head section -->
@@ -17,48 +23,15 @@
 
 <body>
   <header>
-    <!-- example 6 - center on mobile -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <div class="d-flex flex-grow-1">
-          <span class="w-100 d-lg-none d-block">
-            <!-- hidden spacer to center brand on mobile --></span>
-          <a class="navbar-brand d-none d-lg-inline-block" href="#">
-            Scheduling App
-          </a>
-          <a class="navbar-brand-two mx-auto d-lg-none d-inline-block" href="#">
-            <!-- <img src="//placehold.it/40?text=LOGO" alt="logo"> -->
-            <span class="navbar-brand">Scheduling App</span>
-          </a>
-          <div class="w-100 text-right">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          </div>
-        </div>
-        <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
-          <ul class="navbar-nav ml-auto flex-nowrap">
-            <li class="nav-item">
-              <a href="about" class="nav-link m-2 menu-item nav-active">About</a>
-            </li>
-            <li class="nav-item">
-              <a href="features" class="nav-link m-2 menu-item">Features</a>
-            </li>
-            <li class="nav-item m-2">
-              <a href="register">
-                <button class="btn btn-primary my-2 my-sm-0" type="submit">Register</button>
-              </a>
-            </li>
-            <li class="nav-item m-2">
-              <a href="signin">
-                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Sign In</button>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <?php
 
+    if (!$login)
+      include 'nav/main.php';
+    else {
+      include ($admin) ? 'nav/admin.php' : 'nav/student.php';
+    }
+
+    ?>
   </header>
 
 </body>
