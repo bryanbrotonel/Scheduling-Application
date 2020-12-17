8ffe8a9ed6476aec1efb3da284772730e7260780
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2020-12-16 21:51:20 -0800

    Implemented settings styling

--- settings.php
+++ settings.php
@@ -1,7 +1,20 @@
 <?php
 
-session_start();
 include_once("connection/config.php");
+session_start();
+$con = config::connect();
+
+$username = $_SESSION['usersUsername'];
+
+$user = getUserInfo($con, $username);
+
+function getUserInfo($con, $username)
+{
+    $query = $con->prepare("SELECT * FROM users WHERE usersUsername='$username'");
+    $query->execute();
+    
+    return $query->fetch();
+}
 
 include 'header.php';
 include 'src/settings.php';
--- src/settings.php
+++ src/settings.php
@@ -1,13 +1,20 @@
-<div id="main">
-	<h1 class="top">Settings</h1>
-	<div>
-		<button class="btn btn-primary" data-toggle="modal" data-target="#updateUserModal">Update User</button>
-	</div>
-	<div>
+<div class="container">
+	<main class="form-user bg-light rounded border">
+		<form method="post" action="process/process.php">
+			<h1 class="h3 mb-3 fw-normal text-center">Update User</h1>
+			<input type="text" name="username" class="form-control" value="<?php echo $user['usersUsername'] ?>" required autofocus>
+			<input type="text" name="name" class="form-control" value="<?php echo $user['usersName'] ?>" required>
+			<input type="text" name="email" class="form-control" value="<?php echo $user['usersEmail'] ?>" required>
+			<input type="password" name="password" class="form-control" placeholder="Password" required>
+			<input type="hidden" name="type" value="2">
+			<input class="w-100 mt-2 btn btn-lg btn-primary" type="submit" value="Update User" name="update">
+		</form>
 		<?php if ($_SESSION['accountType'] == 2) { ?>
 			<a href='process/deleteuser.php' onclick="return confirm('Are you sure?')">
-				<button class="btn btn-danger">Delete User</button>
+				<button class="w-100 mt-5 btn btn-lg btn-danger">Delete User</button>
 			</a>
-		<?php } ?>
-	</div>
+		<?php }
+
+		?>
+	</main>
 </div>
