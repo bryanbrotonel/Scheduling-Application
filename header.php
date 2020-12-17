7944ff3cd453fd32803ff611449242f3507e97dd
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2020-12-16 20:20:37 -0800

    Implemented user register styling

--- header.php
+++ header.php
@@ -14,10 +14,11 @@ session_start();
   <meta name="viewport" content="width=device-width; initial-scale=1.0;">
   <link rel="stylesheet" type="text/css" href="/book_apps/ch05_guitar_shop/main.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles/main.css" />
 
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="styles/main.css" />
 </head>
 
 <!-- the body section -->
--- src/register.php
+++ src/register.php
@@ -1,14 +1,13 @@
-<div id="main">
-    <h1 class="top">Register</h1>
-</div>
-
-<div>
-    <form method="post" action="process/process.php">
-        <input type="text" name="username" placeholder="Username..." required><br>
-        <input type="password" name="password" placeholder="Password..." required><br>
-        <input type="text" name="name" placeholder="Name..." required><br>
-        <input type="text" name="email" placeholder="Email..." required><br>
-        <input type="hidden" name="type" value="2">
-        <input type="submit" value="Sign Up" name="signup">
-    </form>
+<div class="container">
+    <main class="form-user bg-light rounded border">
+        <form method="post" action="process/process.php">
+            <h1 class="h3 mb-3 fw-normal text-center">Register</h1>
+            <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+            <input type="text" name="name" class="form-control" placeholder="Name" required>
+            <input type="text" name="email" class="form-control" placeholder="Email" required>
+            <input type="password" name="password" class="form-control" placeholder="Password" required>
+            <input type="hidden" name="type" value="2">
+            <input class="w-100 btn btn-lg btn-primary" type="submit" value="Register" name="signup">
+        </form>
+    </main>
 </div>
